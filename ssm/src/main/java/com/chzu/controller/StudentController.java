45594cac8944c9fdddb38d8af1ac79e53427f679
yanxing <lyxtime@163.com>
2015-02-01 22:45:56 +0800

    ssm集成环境搭建好

--- ssm/src/main/java/com/chzu/controller/StudentController.java
+++ ssm/src/main/java/com/chzu/controller/StudentController.java
@@ -1,9 +1,18 @@
 package com.chzu.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author yanxing on 2015/1/28.
  */
 @Controller
 public class StudentController {
+
+    @RequestMapping("/index")
+    public String index(){
+        System.out.println("调转index页面");
+        return "index";
+    }
 
 }
--- ssm/src/main/java/com/chzu/service/impl/StudentServiceImpl.java
+++ ssm/src/main/java/com/chzu/service/impl/StudentServiceImpl.java
@@ -1,9 +1,11 @@
 package com.chzu.service.impl;
 
 import com.chzu.service.StudentService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author yanxing on 2015/1/28.
  */
+@Service("studentService")
 public class StudentServiceImpl implements StudentService {
 }
