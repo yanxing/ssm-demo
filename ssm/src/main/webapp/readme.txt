92de9e7a01cf4df4f4d8bae7de3f4898b795f8ce
yanxing <lyxtime@163.com>
2015-02-05 22:56:01 +0800

    上传ssm.sql文件

--- ssm/src/main/webapp/readme.txt
+++ ssm/src/main/webapp/readme.txt
@@ -1,2 +1,2 @@
-1.采用springmvc4.1.2+spring4.1.2+mybatis3.2.8框架
+1.采用springmvc4.1.2+spring4.1.2+mybatis3.2.8框架 tomcat7+jdk1.7+idea14.0.2
 2.ssm整合demo，有前台界面（基于bootstrap），功能：简单的增删改查（包括关联查询）
--- ssm/src/main/webapp/resources/ssm.sql
+++ ssm/src/main/webapp/resources/ssm.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : local
+Source Server         : localhost
 Source Server Version : 50521
 Source Host           : localhost:3306
 Source Database       : ssm
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50521
 File Encoding         : 65001
 
-Date: 2015-02-03 17:58:06
+Date: 2015-02-05 22:53:50
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,9 +20,9 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `grade`;
 CREATE TABLE `grade` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `grade_id` int(11) NOT NULL,
+  `grade_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`grade_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -36,16 +36,17 @@ INSERT INTO `grade` VALUES ('2', '二班');
 -- ----------------------------
 DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL AUTO_INCREMENT,
   `number` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `sex` varchar(255) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `grade_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`student_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO `student` VALUES ('1', '001', 'lsx', '男', '2015-02-03');
-INSERT INTO `student` VALUES ('2', '002', 'yx', '女', '2015-02-03');
+INSERT INTO `student` VALUES ('1', '001', 'ya1', '男', '2015-02-03', '1');
+INSERT INTO `student` VALUES ('2', '002', 'yx', '女', '2015-02-03', '2');
