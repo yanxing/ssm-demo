e5fb944ff135ec4ba6290f5adf4e3b0237f93b69
李双祥 <lyxtime@163.com>
2015-02-05 17:41:50 +0800

    单元测试

--- ssm/src/test/com/chzu/service/ServiceTest.java
+++ ssm/src/test/com/chzu/service/ServiceTest.java
@@ -3,30 +3,40 @@ package com.chzu.service;
 import com.chzu.model.Student;
 import com.chzu.service.impl.StudentServiceImpl;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
 import javax.inject.Inject;
 import java.util.List;
 
 /**
  * @author yanxing 2015/2/3.
- * 测试业务逻辑层
+ *         测试业务逻辑层
  */
-@ContextConfiguration(locations = {"classpath:spring.xml"})
 public class ServiceTest {
+    protected static ApplicationContext context;
 
-    @Inject
     //@Resource(name="studentService")
     //@Autowired
-    private StudentServiceImpl studentService;
+    //@Inject
+    private static StudentService studentService;
+
+    @BeforeClass
+    public static void beforeClass() {
+        context = new ClassPathXmlApplicationContext("spring.xml");
+        studentService = context.getBean(StudentServiceImpl.class);
+    }
 
     //测试获取所有的学生
     @Test
-    public void testGetAllStudent(){
-        List<Student> students=studentService.getAllStudent();
-        for(Student student:students){
+    public void testGetAllStudent() {
+        List<Student> students = studentService.getAllStudent();
+        for (Student student : students) {
             System.out.println(student);
         }
     }
